1e79a5ca6e118279e38151c20fe423c98f232acb
Fardin96 <fardinshuvro96@gmail.com>
2025-07-19 13:02:50 +0600

    project init, trying out other images

--- node/index.js
+++ node/index.js
@@ -2,15 +2,19 @@ const { createWorker } = require('tesseract.js');
 const path = require('path');
 
 const worker = createWorker({
-  langPath: path.join(__dirname, '..', 'lang-data'), 
-  logger: m => console.log(m),
+  langPath: path.join(__dirname, '..', 'lang-data'),
+  logger: (m) => console.log(m),
 });
 
 (async () => {
   await worker.load();
   await worker.loadLanguage('eng');
   await worker.initialize('eng');
-  const { data: { text } } = await worker.recognize(path.join(__dirname, '..', 'images', 'testocr.png'));
+  const {
+    data: { text },
+  } = await worker.recognize(
+    path.join(__dirname, '..', 'images', 'passport-23to33.jpg')
+  );
   console.log(text);
   await worker.terminate();
 })();
